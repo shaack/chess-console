6ce3c7c97c3cb4e1bc0ad0e0cd0d779d2b27a0c3
Stefan Haack <shaack@gmail.com>
2018-06-25 17:05:36 +0200

    removed imports

--- src/chess-console/ChessConsolePlayer.js
+++ src/chess-console/ChessConsolePlayer.js
@@ -4,9 +4,6 @@
  * License: MIT, see file 'LICENSE'
  */
 
-import {Observe} from "../svjs-observe/Observe.js"
-import {COLOR} from "../cm-chessboard/Chessboard.js"
-
 export class ChessConsolePlayer {
 
     constructor(name, chessConsole) {
