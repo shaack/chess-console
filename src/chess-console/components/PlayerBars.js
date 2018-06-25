c4816ad6cef83e76acf090c070278c5f6c59aec1
Stefan Haack <shaack@gmail.com>
2018-06-25 15:55:05 +0200

    Show players

--- src/chess-console/components/PlayerBars.js
+++ src/chess-console/components/PlayerBars.js
@@ -5,6 +5,8 @@
  */
 
 import {Component} from "../../svjs-app/Component.js"
+import {Observe} from "../../svjs-observe/Observe.js"
+import {COLOR} from "../../cm-chessboard/Chessboard.js"
 
 export class PlayerBars extends Component {
 
@@ -12,6 +14,23 @@ export class PlayerBars extends Component {
         super(module)
         this.topBarElement = this.module.container.querySelector(".player.top")
         this.bottomBarElement = this.module.container.querySelector(".player.bottom")
+        Observe.property(this.module.view.chessboard.state, "orientation", () => {
+            this.redraw()
+        })
+        this.redraw()
+    }
+
+    redraw() {
+        clearTimeout(this.redrawDebounce)
+        this.redrawDebounce = setTimeout(() => {
+            if(this.module.view.chessboard.getOrientation() === COLOR.white) {
+                this.bottomBarElement.innerHTML = this.module.player.name
+                this.topBarElement.innerHTML = this.module.opponent.name
+            } else {
+                this.bottomBarElement.innerHTML = this.module.opponent.name
+                this.topBarElement.innerHTML = this.module.player.name
+            }
+        })
     }
 
 }
