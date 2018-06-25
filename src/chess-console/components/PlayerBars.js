924637f7fd30585b3719dec631fb2680018d5951
Stefan Haack <shaack@gmail.com>
2018-06-25 15:40:36 +0200

    update svjs-app

--- src/chess-console/components/PlayerBars.js
+++ src/chess-console/components/PlayerBars.js
@@ -3,13 +3,15 @@
  * Repository: https://github.com/shaack/chess-console
  * License: MIT, see file 'LICENSE'
  */
+
 import {Component} from "../../svjs-app/Component.js"
 
 export class PlayerBars extends Component {
 
-    constructor(topBarElement, bottomBarElement) {
-        super(null)
-        this.topBarElement = topBarElement
-        this.bottomBarElement = bottomBarElement
+    constructor(module) {
+        super(module)
+        this.topBarElement = this.module.container.querySelector(".player.top")
+        this.bottomBarElement = this.module.container.querySelector(".player.bottom")
     }
+
 }
