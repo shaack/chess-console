990eb3c493017d11b4f63240de9c8885e50c8c7e
Stefan Haack <shaack@gmail.com>
2018-06-26 11:37:50 +0200

    Before separating board component

--- src/chess-console/ChessConsoleView.js
+++ src/chess-console/ChessConsoleView.js
@@ -71,17 +71,17 @@ export class ChessConsoleView {
     }
 
     render() {
-        const colsetConsoleGame = "console-game col-lg-7 order-lg-2 col-md-8 order-md-1 order-sm-1 col-sm-12 order-sm-1"
-        const colsetConsoleControls = "console-controls col-lg-3 order-lg-3 col-md-4 order-md-2 col-sm-8 order-sm-3"
-        const colsetConsoleStatus = "console-status col-lg-2 order-lg-1 order-md-3 col-sm-4 order-sm-2"
+        const colsetConsoleGame = "col-lg-7 order-lg-2 col-md-8 order-md-1 order-sm-1 col-sm-12 order-sm-1"
+        const colsetConsoleControls = "col-lg-3 order-lg-3 col-md-4 order-md-2 col-sm-8 order-sm-3"
+        const colsetConsoleStatus = "col-lg-2 order-lg-1 order-md-3 col-sm-4 order-sm-2"
         this.container.innerHTML =
             `<div class="row chess-console">
-                <div class="${colsetConsoleGame}">
+                <div class="console-board ${colsetConsoleGame}">
                     <div class="player top">&nbsp;</div>
                     <div class="chessboard"></div>
                     <div class="player bottom">&nbsp;</div>
                 </div>
-                <div class="${colsetConsoleControls}">
+                <div class="console-controls ${colsetConsoleControls}">
                     <div class="control-icons">
                         <span class="board-control"></span>
                         <span class="game-control"></span>
@@ -89,7 +89,7 @@ export class ChessConsoleView {
                     <div class="game-status"></div>
                     <div class="last-error"></div>
                 </div>
-                <div class="${colsetConsoleStatus}">
+                <div class="console-status ${colsetConsoleStatus}">
                     <div class="history"></div>
                     <div class="captured-pieces"></div>
                     <div class="engine-status"></div>
---
+++ src/chess-console/components/Board.js
@@ -0,0 +1,6 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/chess-console
+ * License: MIT, see file 'LICENSE'
+ */
+
