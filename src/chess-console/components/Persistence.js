6b21594859a1e073d32fdefa58c3b8f5a3978b11
Stefan Haack <shaack@gmail.com>
2018-07-31 11:17:59 +0200

    Fixed Persistence Component

--- src/chess-console/components/Persistence.js
+++ src/chess-console/components/Persistence.js
@@ -3,6 +3,7 @@
  * Repository: https://github.com/shaack/chess-console
  * License: MIT, see file 'LICENSE'
  */
+
 import {Component} from "../../svjs-app/Component.js"
 import {MESSAGE} from "../ChessConsole.js"
 import {COLOR} from "../../cm-chesstools/ChessTools.js"
@@ -17,16 +18,18 @@ export class Persistence extends Component {
     }
 
     load(user) {
+        console.log("load", user)
         this.prefix = user + "-"
         try {
-            if (localStorage.getItem(this.props.prefix + "playerColor") !== null) {
+            if (localStorage.getItem(this.prefix + "playerColor") !== null) {
                 this.module.state.playerColor = JSON.parse(localStorage.getItem(this.prefix + "playerColor"))
             } else {
+                console.log("starting new game")
                 this.module.startGame(COLOR.white)
             }
-            if (localStorage.getItem(this.props.prefix + "pgn") !== null) {
+            if (localStorage.getItem(this.prefix + "pgn") !== null) {
                 this.module.state.chess.load_pgn(localStorage.getItem(this.prefix + "pgn"))
-                this.module.state.plyViewed = this.module.state.plyCount
+                this.module.state.plyViewed = this.module.state.plyCount()
             }
         } catch (e) {
             localStorage.clear()
@@ -35,7 +38,7 @@ export class Persistence extends Component {
     }
 
     save() {
-        localStorage.setItem(this.prefix + "playerColor", JSON.stringify(this.module.state.playerColor));
-        localStorage.setItem(this.prefix + "pgn", this.module.state.chess.pgn());
+        localStorage.setItem(this.prefix + "playerColor", JSON.stringify(this.module.state.playerColor))
+        localStorage.setItem(this.prefix + "pgn", this.module.state.chess.pgn())
     }
 }
