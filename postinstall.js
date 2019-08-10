7c46cb1c241aa500e90f71ecc49169818c0006a2
Stefan Haack <shaack@gmail.com>
2019-08-10 14:19:59 +0200

    bugfix

--- postinstall.js
+++ postinstall.js
@@ -3,18 +3,10 @@
  * License: MIT, see file 'LICENSE'
  */
 
-const LibraryManager = require("./node_modules/cm-web-modules/src/LibraryManager.js")
+const LibraryManager = require("cm-web-modules/src/LibraryManager.js")
 const manager = new LibraryManager(__dirname)
 
 manager.addProject("cm-web-modules")
-/*
-manager.addModule("svjs-utils")
-manager.addModule("svjs-i18n")
-manager.addModule("svjs-message-broker")
-manager.addModule("svjs-svg")
-manager.addModule("svjs-observe")
-manager.addModule("svjs-audio")
-*/
 manager.addProject("cm-chessboard")
 manager.addProject("cm-chesstools")
 
